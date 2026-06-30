ffba9f58bf1e01fc813b962f457c4decbbe632ae
amr <amr.draz.2009@gmail.com>
2026-06-30 13:37:01 +0300

    handle login error

--- src/pages/Signup/VerificationForm.jsx
+++ src/pages/Signup/VerificationForm.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { useNavigate } from "react-router-dom";
+import { UserContext } from './../../context/UserContext';
 
 
 function VerificationForm({ email}) {
--- src/pages/profile/ProfilePages/wishlist/Wishlist.jsx
+++ src/pages/profile/ProfilePages/wishlist/Wishlist.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Course from '../../../../components/Course'
 
 function Wishlist() {
   return (
-    <div>
-      
+    <div className='wish_list'>
+      <h1>Your Wishlist</h1>
+      {[...Array(10)].map((_, index) => (
+        <Course />
+      ))}
     </div>
   )
 }
